bf9af321861b172e7e373fe383d5fc5899870932
DRTK <ritthikai2544@gmail.com>
2022-08-19 17:40:48 +0700

    part 4 lab commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import './App.css';
 import Box from './box';
   function Game() {
 
-const [turn, setTurn] = useState('X')
+  const [turn, setTurn] = useState('X')
+  const board = [[], [], []]
 
-    function changeTurn(){
+    function changeTurn(row, col){
+
+        board[row][col] = turn
+
         setTurn(turn => turn == 'X' ? 'O' :'X')
+        console.log(board)
     }
     return <div id ="game">
       <div><h1>TIC TAC TOC GAME</h1></div>
       <br />
       <br />
       <div className="row">
-          <Box id={0} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={1} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={2} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={0} col={0} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={0} col={1} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={0} col={2} currentState = {turn} changeTurn={changeTurn}/>
       </div>
 
       <div className="row">
-          <Box id={3} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={4} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={5}currentState =  {turn} changeTurn={changeTurn}/>
+          <Box row={1} col={3} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={1} col={4} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={1} col={5}currentState =  {turn} changeTurn={changeTurn}/>
       </div>
 
       <div className="row">
-          <Box id={6} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={7} currentState = {turn} changeTurn={changeTurn}/>
-          <Box id={8} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={2} col={6} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={2} col={7} currentState = {turn} changeTurn={changeTurn}/>
+          <Box row={2} col={8} currentState = {turn} changeTurn={changeTurn}/>
       </div>
     </div>
   }
--- src/box.js
+++ src/box.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import './App.css';
 
 function Box(props){
     const [text, setText] = useState('')
     function toggletext(){
         if(text === ''){
             setText(props.currentState)
-            props.changeTurn()
+            props.changeTurn(props.row, props.col)
         }
 
     } 
